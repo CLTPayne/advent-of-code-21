34e6e251032dc6bd5bb3e44660f8109eff518d4c
Charlotte Payne <charlotte.payne@ft.com>
2021-12-22 14:35:39 +0100

    Day 10, Part 2

--- 10-12-2021.js
+++ 10-12-2021.js
@@ -24,47 +24,47 @@ console.log({ data });
 // end of the array. Check that the character being popped of is
 // the correct corresponding closing bracket. Otherwise it's an
 // incorrect closer
-const findFirstIncorrectClosingBracket = (data) => {
-	const openings = ["(", "[", "{", "<"];
-	const correspondingClosings = {
-		")": "(",
-		"]": "[",
-		"}": "{",
-		">": "<",
-	};
-	const openingsFound = [];
-	const incorrectClosingsFound = [];
-	for (let i = 0; i < data.length; i++) {
-		for (let j = 0; j < data[i].length; j++) {
-			// Is it an opening character?
-			if (openings.includes(data[i][j])) {
-				openingsFound.push(data[i][j]);
-				// Does it close the previous opening character?
-			} else if (
-				openingsFound[openingsFound.length - 1] ===
-				correspondingClosings[data[i][j]]
-			) {
-				openingsFound.pop(data[i][j]);
-			} else {
-				incorrectClosingsFound.push(data[i][j]);
-				break;
-			}
-		}
-	}
-	const scores = {
-		")": 3,
-		"]": 57,
-		"}": 1197,
-		">": 25137,
-	};
-	return incorrectClosingsFound.reduce(
-		(sum, character) => sum + scores[character],
-		0
-	);
-};
+// const findFirstIncorrectClosingBracket = (data) => {
+// 	const openings = ["(", "[", "{", "<"];
+// 	const correspondingClosings = {
+// 		")": "(",
+// 		"]": "[",
+// 		"}": "{",
+// 		">": "<",
+// 	};
+// 	const openingsFound = [];
+// 	const incorrectClosingsFound = [];
+// 	for (let i = 0; i < data.length; i++) {
+// 		for (let j = 0; j < data[i].length; j++) {
+// 			// Is it an opening character?
+// 			if (openings.includes(data[i][j])) {
+// 				openingsFound.push(data[i][j]);
+// 				// Does it close the previous opening character?
+// 			} else if (
+// 				openingsFound[openingsFound.length - 1] ===
+// 				correspondingClosings[data[i][j]]
+// 			) {
+// 				openingsFound.pop(data[i][j]);
+// 			} else {
+// 				incorrectClosingsFound.push(data[i][j]);
+// 				break;
+// 			}
+// 		}
+// 	}
+// 	const scores = {
+// 		")": 3,
+// 		"]": 57,
+// 		"}": 1197,
+// 		">": 25137,
+// 	};
+// 	return incorrectClosingsFound.reduce(
+// 		(sum, character) => sum + scores[character],
+// 		0
+// 	);
+// };
 
-const result = findFirstIncorrectClosingBracket(data);
-console.log({ result });
+// const result = findFirstIncorrectClosingBracket(data);
+// console.log({ result });
 
 // Status:
 // Complete
@@ -78,3 +78,68 @@ console.log({ result });
 // end of the array. Check that the character being popped of is
 // the correct corresponding closing bracket. Otherwise it's an
 // incorrect closer
+
+const findUnfinishedLines = (data) => {
+	const openings = ["(", "[", "{", "<"];
+	const correspondingClosings = {
+		")": "(",
+		"]": "[",
+		"}": "{",
+		">": "<",
+		"(": ")",
+		"[": "]",
+		"{": "}",
+		"<": ">",
+	};
+	const scores = {
+		")": 1,
+		"]": 2,
+		"}": 3,
+		">": 4,
+	};
+	let openingsFound = [];
+	let completingString = [];
+	const totals = []
+	for (let i = 0; i < data.length; i++) {
+		for (let j = 0; j <= data[i].length; j++) {
+			// If it is an opening character add it to the list of openings
+			if (openings.includes(data[i][j])) {
+				openingsFound.push(data[i][j]);
+				// If it's a closing character does it close the previous opening character
+			} else if (
+				openingsFound[openingsFound.length - 1] ===
+				correspondingClosings[data[i][j]]
+			) {
+				openingsFound.pop();
+				// If it's an incorrect closing, reset and skip the line!
+			} else if (openingsFound[openingsFound.length - 1] !== 
+				correspondingClosings[data[i][j]] && data[i][j]) {
+					// console.log("corruptedLines:",{line: data[i], value: data[i][j]})
+					openingsFound = [];
+					break;
+			} else if (data[i][j] === undefined) {
+				const charactersToFind = openingsFound.length
+				// Loop over the openingsFound add add the correct closing character to finish the line
+				for (let k = 0; k < charactersToFind; k++) {
+					completingString.push(correspondingClosings[openingsFound.pop()])
+				}
+				openingsFound = [];
+				const lineScore = completingString.reduce(
+					(sum, character) => (sum * 5) + scores[character],
+					0
+				);
+				totals.push(lineScore);
+				completingString = [];
+			}
+		}
+
+	}
+	const sortedTotals = totals.sort((a, b) => a - b);
+	return sortedTotals[Math.floor(sortedTotals.length / 2)]
+};
+
+const result = findUnfinishedLines(data);
+console.log({ result });
+
+// Status:
+// Complete
